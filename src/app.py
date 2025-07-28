4e5de0ed4c124fde74138c7f9abc0a35f7565003
ceco10595 <caroline.eco@pepsico.com>
2025-07-28 10:24:48 -0400

    model changes

--- src/app.py
+++ src/app.py
@@ -1,8 +1,8 @@
 # pyright: reportAttributeAccessIssue=false, reportArgumentType=false
 # src/app.py
-#__import__('pysqlite3') 
-#import sys 
-#sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3') 
+import sys 
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import re
 import csv
